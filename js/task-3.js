0a0e0bf225d9b641bbc5c158ad45df14a81a07cf
Serhii-Korsh <167204860+Serhii-Korsh@users.noreply.github.com>
2024-07-29 15:03:51 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -13,3 +13,16 @@ console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
+function getExtremeScores(scores) {
+   const best = Math.max(...scores);
+    const worst = Math.min(...scores);
+    return { best, worst };  }
+
+
+console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
+console.log(getExtremeScores([19, 7, 4, 17, 81, 24]));
+
+
+
+
+
